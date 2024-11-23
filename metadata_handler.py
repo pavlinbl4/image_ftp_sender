6c0aa63bd2367ed9f1b7dc3d931c83de92d9567d
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-11-23 22:34:18 +0300

    added  logger

--- metadata_handler.py
+++ metadata_handler.py
@@ -1,19 +1,25 @@
 # pip install PyExifTool
 import exiftool
+from loguru import logger
 
+# Удаляем стандартный обработчик
+logger.remove()
+
+"""
+    Извлекает значения указанных тегов из изображения.
+
+    :param file_path: Путь к файлу изображения.
+    :param tags: Список тегов, которые нужно получить.
+    :return: Словарь с тегами и их значениями.
+    """
 
 def get_image_metadata(file_path,
                       tags ):
-    """
-        Извлекает значения указанных тегов из изображения.
-
-        :param file_path: Путь к файлу изображения.
-        :param tags: Список тегов, которые нужно получить.
-        :return: Словарь с тегами и их значениями.
-        """
+    logger.info(file_path)
     with exiftool.ExifToolHelper() as et:
         result = {}
         for tag in tags:
+            logger.info(tag)
             metadata = et.get_tags(file_path, tag)
             if metadata and tag in metadata[0]:
                 result[tag] = metadata[0][tag]
@@ -35,7 +41,8 @@ def clear_exif(file_path):
 
 
 if __name__ == '__main__':
-    file_path = './test_image/20241109PEV_8316.JPG'
+    # file_path = './test_image/20241109PEV_8316.JPG'
+    file_path = '/Users/evgeniy/Desktop/Images_for_FTP_batch/20241122PEV_9723.jpg'
     clear_exif(file_path)
     print(get_image_metadata(file_path,
                              tags=['XMP:Description', 'XMP:Label', 'XMP:Creator']))
