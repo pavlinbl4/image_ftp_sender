801bcbc07e23a7630006316d6b7d8aa78ecb0edb
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-11-23 22:33:57 +0300

    added  type extension control

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os
 from db_handler import initialize_database, is_file_sent
 from ftp_uploader import upload_file_to_multiple_ftps
 from metadata_handler import get_image_metadata, clear_exif
+from loguru import logger
+
+# Удаляем стандартный обработчик
+logger.remove()
 
 
 def main():
@@ -17,14 +21,16 @@ def main():
     ftp_details = config['ftp_details']
 
     for file_name in os.listdir(image_dir):
-        file_path = os.path.join(image_dir, file_name)
-        metadata = get_image_metadata(file_path,
-                                      tags=['XMP:Description', 'XMP:Label'])
+        logger.info(file_name)
+        if file_name.lower().endswith('jpg'):
+            file_path = os.path.join(image_dir, file_name)
+            metadata = get_image_metadata(file_path,
+                                          tags=['XMP:Description', 'XMP:Label'])
 
-        if metadata.get('XMP:Label') == 'Green' and metadata.get('XMP:Description'):
-            if not is_file_sent(file_name):
-                clear_exif(file_path)
-                upload_file_to_multiple_ftps(file_path, ftp_details)
+            if metadata.get('XMP:Label') == 'Green' and metadata.get('XMP:Description'):
+                if not is_file_sent(file_name):
+                    clear_exif(file_path)
+                    upload_file_to_multiple_ftps(file_path, ftp_details)
 
 
 if __name__ == "__main__":
